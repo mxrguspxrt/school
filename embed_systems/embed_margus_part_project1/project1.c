236e082b99f7fe6cec4eb0bb0b8037545a797e62
mxrguspxrt <margus@tione.eu>
2014-01-10 23:30:51 +0200

    Embed systems: Project1 work in C

---
+++ embed_systems/embed_margus_part_project1/project1.c
@@ -0,0 +1,50 @@
+#include <8051.h>
+
+int button_number_get();
+void display_number_set(int number);
+void run_sleep(int milliseconds);
+void run_program();
+
+int button_number_get(){
+  int active = 255 ^ P0;
+  int position_values[] = {0, 1, 2, 4, 8, 16, 32, 64};
+  int position;
+  for(position=0; position < sizeof(position_values); position++){
+    if(position_values[position]==active){
+      return position;
+    }
+  }
+  return 0;
+}
+
+void display_number_set(int number){
+  // multiplexed LED display + 8 segment editor to construct numbers
+  // 1-00000110-6
+  // 2-01011011-91
+  // 3-01001111-79
+  // 4-01100110-102
+  // 5-01101101-109
+  // 6-01111101-125
+  // 7-00000111-7
+  // 8-01111111-127
+  // 9-01101111-111
+  // .-10000000-128
+  int numbers[] = {128,6,91,79,102,109,125,7,127,111};
+  P1 = 255 ^ numbers[number];
+}
+
+void run_sleep(int milliseconds){
+  int i;
+  for(i=0; i<milliseconds; i++){
+  }
+}
+
+void run_program(){
+  while(1){
+    display_number_set(button_number_get());
+  };
+}
+
+void main(){
+  run_program();
+}
---
+++ embed_systems/embed_margus_part_project1/project1_hw.vhw
@@ -0,0 +1,5 @@
+# MCU 8051 IDE: Virtual HW configuration file
+# Project: embed_margus_part_project1
+
+LedDisplay {{4 1 0 1 5 1 1 1 6 1 2 1 7 1 3 1} {4 3 0 7 5 2 1 6 6 1 2 5 7 0 3 4} 230x175+54+427 {} red 1 0}
+SimpleKeyPad {{4 0 0 0 5 0 1 0 6 0 2 0 7 0 3 0} {4 3 0 7 5 2 1 6 6 1 2 5 7 0 3 4} 265x125+56+98 {} {4 0 0 0 5 0 1 0 6 0 2 0 7 0 3 0} 1 0}
