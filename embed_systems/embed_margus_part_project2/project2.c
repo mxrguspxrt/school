82295fd92ffa2775fca700c02d4ba4aae1925bda
mxrguspxrt <margus@tione.eu>
2014-01-11 16:44:11 +0200

    EmbedSystems Project2 anode to cathode, fixed bugs in code. WORKING! YEYE

--- embed_systems/embed_margus_part_project2/project2.c
+++ embed_systems/embed_margus_part_project2/project2.c
@@ -1,5 +1,7 @@
 #include <8051.h>
 
+// Author mxrguspxrt 20140111
+
 int car_is_entering();
 int car_is_leaving();
 void display_value_set(int value);
@@ -20,7 +22,7 @@ int car_is_leaving(){
   return P0_1 == 0;
 }
 
-void display_number_set(int number){
+void display_number_set(int column, int number){
   // multiplexed LED display + 8 segment editor to construct numbers
   // 1-00000110-6
   // 2-01011011-91
@@ -33,35 +35,27 @@ void display_number_set(int number){
   // 9-01101111-111
   // 0-00111111-63
   int numbers[] = {63,6,91,79,102,109,125,7,127,111};
-  P1 = 255 ^ numbers[number]; // cathode vs anode - should be redone if using anode
+  int columns[] = {1, 2, 4};
+  P2 = columns[column];
+  P1 = numbers[number]; // cathode vs anode - should be redone if using anode 255 ^ 
 }
 
 void display_value_set(int value){
   int column1   = value%10;
   int column10  = (value%100-column1)/10;
-  int column100 = (value%1000-value%100)/100;
+  int column100 = (value%1000-column10)/100;
 
-  P2 = 6;
-  display_number_set(column1);
-  sleep(3);
-  P2 = 7;
 
-  P2 = 5;  
-  display_number_set(column10);
-  sleep(3);
-  P2 = 7;
-
-  P2 = 3; 
-  display_number_set(column100);
-  sleep(3);
-  P2 = 7;
+  display_number_set(0, column1);
+  display_number_set(1, column10);
+  display_number_set(2, column100);
 }
 
 void run(){
   // cars_parked - how many cars ar parked at the moment
   // car_is_entering - if it was entering and is entering, do not register it twice
   // car_is_leaving - car is leaving=1 until leaving sensor says that it has left
-  int cars_parked = 123;
+  int cars_parked = 0;
   int car_was_entering = 0; 
   int car_was_leaving = 0;
   while(1){
@@ -76,7 +70,7 @@ void run(){
     if(car_is_leaving()){
       if(!car_was_leaving){
         car_was_leaving = 1;
-        if(cars_parked>=0){
+        if(cars_parked>0){
           cars_parked--;
         }
       }
