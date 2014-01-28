2e03db2003de34bbe65d8eb60a94aa13d070353b
Margus Pxrt <margus@wave.ee>
2014-01-29 01:15:55 +0200

    Added game ends when someone wins

--- csharp/Trips/Trips/Gameplay.cs
+++ csharp/Trips/Trips/Gameplay.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,30 +13,29 @@ namespace Trips
     {
         public string[] selections;
         public string currentSelection;
-        public int[,] winningRows;
+        public List<int[]> winningRows;
 
         public Gameplay()
         {
             currentSelection = "o";
             selections = new string[9];
-            winningRows = new int[,]{
-                {0,1,2},
-                {3,4,5},
-                {6,7,8},
-                {0,3,5},
-                {1,4,7},
-                {2,5,8},
-                {0,4,8},
-                {2,4,6}
-            };
+            winningRows = new List<int[]>();
+            winningRows.Add(new int[]{0,1,2});
+            winningRows.Add(new int[]{3,4,5});
+            winningRows.Add(new int[]{6,7,8});
+            winningRows.Add(new int[]{0,3,5});
+            winningRows.Add(new int[]{1,4,7});
+            winningRows.Add(new int[]{2,5,8});
+            winningRows.Add(new int[]{0,4,8});
+            winningRows.Add(new int[]{2,4,6});
         }
 
-        public bool selectSlot(int number)
+        public bool selectSlot(int position)
         {
-            if (selections[number] == null)
+            if (selections[position] == null && !hasFinished())
             {
-                selections[number] = toggleSelection();
-                Debug.WriteLine("Chose " + number + " " + selections[number]);
+                selections[position] = toggleSelection();
+                Debug.WriteLine("Chose " + position + " " + selections[position]);
                 return true;
             } else
             {
@@ -49,13 +49,43 @@ namespace Trips
             return currentSelection;
         }
 
+        public string[] getSelectionsValues(int[] positions)
+        {
+            string[] values = new string[positions.Length];
+            for (int i=0; i<positions.Length; i+=1)
+            {
+                values[i] = selections[positions[i]];
+            }
+            return values;
+        }
+
+        public bool allAreEqualAndNotNulls(string[] characters)
+        {
+            string last = null;
+            for (int i = 0; i < characters.Length; i += 1)
+            {
+                if (characters[i] == null)
+                {
+                    return false;
+                }
+                if (i != 0 && characters[i] != last)
+                {
+                    return false;
+                }
+                last = characters[i];
+            }
+            return true;
+        }
+
         public bool hasFinished()
         {
-            for(int x=0; x<winningRows.GetLength(0); x+=1)
+            foreach (int[] winningRow in winningRows)
             {
-                for (int y = 0; y < winningRows.GetLength(1); y+=1)
+                string[] selectionValues = getSelectionsValues(winningRow);
+                bool areEqual = allAreEqualAndNotNulls(selectionValues);
+                if (areEqual)
                 {
-                    Debug.WriteLine(winningRows[x,y]);
+                    return true;
                 }
             }
             return false;
