1cf19b0dbe35859ffe887a88fd2a8cb4c0f30c75
Margus Pxrt <margus@wave.ee>
2014-01-29 00:03:50 +0200

    csharp

---
+++ csharp/Trips/Trips/Gameplay.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.ComponentModel;
+
+namespace Trips
+{
+    public class Gameplay
+    {
+        public string[] selections;
+        public string currentSelection;
+        public int[,] winningRows;
+
+        public Gameplay()
+        {
+            currentSelection = "o";
+            selections = new string[9];
+            winningRows = new int[,]{
+                {0,1,2},
+                {3,4,5},
+                {6,7,8},
+                {0,3,5},
+                {1,4,7},
+                {2,5,8},
+                {0,4,8},
+                {2,4,6}
+            };
+        }
+
+        public bool selectSlot(int number)
+        {
+            if (selections[number] == null)
+            {
+                selections[number] = toggleSelection();
+                Debug.WriteLine("Chose " + number + " " + selections[number]);
+                return true;
+            } else
+            {
+                return false;
+            }
+        }
+
+        public string toggleSelection()
+        {
+            currentSelection = currentSelection=="o" ? "x" : "o";
+            return currentSelection;
+        }
+
+        public bool hasFinished()
+        {
+            for(int x=0; x<winningRows.GetLength(0); x+=1)
+            {
+                for (int y = 0; y < winningRows.GetLength(1); y+=1)
+                {
+                    Debug.WriteLine(winningRows[x,y]);
+                }
+            }
+            return false;
+        }
+
+    }
+}
---
+++ csharp/Trips/Trips/MainPage.xaml.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace Trips
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+
+        public Gameplay gameplay;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            this.Loaded += MainPage_Loaded;
+        }
+
+        void MainPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            gameplay = new Gameplay();
+            this.DataContext = gameplay;
+        }
+
+        private void gameButton_Click(int slot, object sender)
+        {
+            gameplay.selectSlot(slot);
+            ((Button)sender).Content = gameplay.selections[slot];
+        }
+
+
+        private void slot0_Click(object sender, RoutedEventArgs e)
+        {
+            gameButton_Click(0, sender);
+        }
+
+
+        private void slot1_Click(object sender, RoutedEventArgs e)
+        {
+            gameButton_Click(1, sender);
+        }
+
+
+        private void slot2_Click(object sender, RoutedEventArgs e)
+        {
+            gameButton_Click(2, sender);
+        }
+
+
+        private void slot3_Click(object sender, RoutedEventArgs e)
+        {
+            gameButton_Click(3, sender);
+        }
+
+        private void slot4_Click(object sender, RoutedEventArgs e)
+        {
+            gameButton_Click(4, sender);
+        }
+
+        private void slot5_Click(object sender, RoutedEventArgs e)
+        {
+            gameButton_Click(5, sender);
+        }
+
+        private void slot6_Click(object sender, RoutedEventArgs e)
+        {
+            gameButton_Click(6, sender);
+        }
+
+
+        private void slot7_Click(object sender, RoutedEventArgs e)
+        {
+            gameButton_Click(7, sender);
+        }
+
+
+        private void slot8_Click(object sender, RoutedEventArgs e)
+        {
+            gameButton_Click(8, sender);
+        }
+
+        
+        private void reset_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
